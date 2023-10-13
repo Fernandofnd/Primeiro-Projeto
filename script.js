933b16d07e4703d4c2ebefe27bd6073f555392c1
Fernando Mendes <fernando.tunado@gmail.com>
2023-10-12 23:20:21 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggleMode() {
+  const html = document.documentElement
+
+  /*if(html.classList.contains('Light')) {
+    html.classlist.contains('Light')
+  } else {
+    html.classList.add('Light')
+  }*/
+
+  html.classList.toggle("Light")
+}
